dd66e7003a39312e4fd3ddeaa60fe402aab34400
IrinaAginskaya <aginska@gmail.com>
2021-12-21 21:44:52 +0200

    add task

---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import shortid from 'shortid';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <>
+      {options.map(option => (
+        <button key={shortid.generate()} type="button" name={option} onClick={onLeaveFeedback}>
+          {option}
+        </button>
+      ))}
+    </>
+  );
+};
+FeedbackOptions.propTypes = {
+  options: PropTypes.array,
+  onLeaveFeedback: PropTypes.func,
+};
+export default FeedbackOptions;
